e24965adb92e7a507dd47312cc7941277273baea
vkirkl <georgetownpolecat@gmail.com>
2026-04-06 12:43:52 -0700

    Adding properties override dir.

--- src/main/java/org/ecocean/JsonProperties.java
+++ src/main/java/org/ecocean/JsonProperties.java
@@ -1,18 +1,20 @@
 package org.ecocean;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Properties;
 import org.apache.commons.io.IOUtils;
 
 public class JsonProperties extends Properties {
-    private static final String propertiesOverrideDir =
-        "/data/wildbook_data_dir/WEB-INF/classes/bundles";
+    private static final Logger logger = LogManager.getLogger(JsonProperties.class);
     private static final String propertiesDir = "WEB-INF/classes/bundles";
     private static final String jsonLinkPrefix = "@";
 
@@ -20,15 +22,13 @@ public class JsonProperties extends Properties {
     private String fullPath;
     private JSONObject json;
 
-    public JsonProperties(String fname)
-    throws FileNotFoundException {
+    public JsonProperties(String fname) {
         try {
             this.setFname(fname);
             this.loadFullPath();
             this.setJson(fromFile(this.getFullPath()));
         } catch (Exception e) {
-            System.out.println("Hit an exception on new JsonProperties:");
-            e.printStackTrace();
+            logger.error("Failed to initialize JsonProperties for {}", fname, e);
         }
     }
 
@@ -57,35 +57,28 @@ public class JsonProperties extends Properties {
     }
 
     public Object get(String periodSeparatedKeys) {
-        // System.out.println("get called on "+periodSeparatedKeys);
         try {
             String[] keys = periodSeparatedKeys.split("\\.");
-            String keyPrintable = String.join("->", keys);
-            // System.out.println("get parsed keys "+keyPrintable);
             return getRecursive(keys, this.getJson());
         } catch (Exception e) {
-            System.out.println("JsonProperties.get hit an exception on key " + periodSeparatedKeys);
-            System.out.println(" ... searching json " + fname);
-            e.printStackTrace();
+            logger.error("JsonProperties.get hit an exception on key {} searching json {}",
+                periodSeparatedKeys, fname, e);
             return null;
         }
     }
 
     public Object getRecursive(String[] keys, JSONObject currentLevel) {
-        // System.out.println("getRecursive called on "+String.join(".", keys));
-
         String key = keys[0];
         String linkDestination = getLinkDestination(key, currentLevel);
         boolean followLink = (linkDestination != null);
 
-        // if (followLink) System.out.println("getRecursive following a link to "+linkDestination);
         // base case
         if (keys.length == 1) {
             return (followLink ? get(linkDestination) : currentLevel.get(key));
         }
         // multiline ternary : nextLevel depends on whether we are following a link at this level
-        JSONObject nextLevel = followLink ? (JSONObject)this.get(
-            linkDestination) : (JSONObject)currentLevel.get(key);
+        JSONObject nextLevel = followLink ? (JSONObject) this.get(
+            linkDestination) : (JSONObject) currentLevel.get(key);
         String[] nextKeys = Arrays.copyOfRange(keys, 1, keys.length);
         return getRecursive(nextKeys, nextLevel);
     }
@@ -95,41 +88,46 @@ public class JsonProperties extends Properties {
         try {
             String val = jobj.getString(key);
             if (val.startsWith(jsonLinkPrefix)) return val.substring(1);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            logger.trace("Key {} not found or not a string link in JSON", key);
+        }
         return null;
     }
 
     // sets full path to the file defined by this.fname, looking first in overrideDir then the default propertiesDir
     public void loadFullPath()
     throws FileNotFoundException {
-        if (Util.fileExists(overrideFilepath())) {
-            this.setFullPath(overrideFilepath());
-        } else if (Util.fileExists(defaultFilepath())) {
-            this.setFullPath(defaultFilepath());
+        String overridePath = overrideFilepath();
+        String defaultPath = defaultFilepath();
+        if (Util.fileExists(overridePath)) {
+            logger.debug("Loading {} from override path {}", fname, overridePath);
+            this.setFullPath(overridePath);
+        } else if (Util.fileExists(defaultPath)) {
+            logger.debug("Loading {} from default path {}", fname, defaultPath);
+            this.setFullPath(defaultPath);
         } else {
             throw new FileNotFoundException("Could not find file " + fname + " in default (" +
-                    defaultFilepath() + ") or override (" + overrideFilepath() + ") directories");
+                    defaultPath + ") or override (" + overridePath + ") directories");
         }
     }
 
     public static JSONObject fromFile(String fullPath) {
-        File f = new File(fullPath);
         JSONObject json = null;
 
         try {
             InputStream is = new FileInputStream(fullPath);
-            String jsonTxt = IOUtils.toString(is, "UTF-8");
-            // System.out.println("JSONProperties successfully parsed "+fullPath);
+            String jsonTxt = IOUtils.toString(is, StandardCharsets.UTF_8);
             json = new JSONObject(jsonTxt);
         } catch (Exception e) {
-            System.out.println("Hit an exception on JsonProperties.fromFile(" + fullPath + ")");
-            e.printStackTrace();
+            logger.error("Failed to load JSON from {}", fullPath, e);
         }
         return json;
     }
 
     private String overrideFilepath() {
-        return propertiesOverrideDir + File.separator + getFname();
+        return CommonConfiguration.getWildbookDataDir()
+            + File.separator + "WEB-INF" + File.separator + "classes"
+            + File.separator + "bundles" + File.separator + getFname();
     }
 
     private String defaultFilepath() {
